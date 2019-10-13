154f46c2e5052e10867f1f65207344271a843902
tonyupup <z-l-8@hotmail.com>
2019-10-13 10:11:09 +0800

    del

--- vad/main.c
+++
@@ -1,6 +0,0 @@
-extern int split(const char *,const char *);
-
-
-int main(int argc,char *argv[]){
-    return split(argv[1],argv[2]);
-}
--- vad/valgrind.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-FILE=build/vad-demo
-valgrind --leak-check=full --show-leak-kinds=all --show-reachable=no --track-origins=yes $FILE
